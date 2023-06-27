39b97c5b54257be44a136b9a9a7122719ee73434
nvdiw <nouraeinavid@gmail.com>
2023-06-27 17:07:39 +0430

    added all word , all letter counter

--- string_counter.py
+++ string_counter.py
@@ -1,10 +1,12 @@
 import json
 
-s = 'hi my name is navid and i want to test this string .'
+s = 'hi my name is navid and i want to test this string this code shows you how many letters and words you type i dont know what i write but i should write somthing and how are you what are you doing what happend yesterday i hope you like me.'
 lst_s = s.split()
+len_all_words = len(lst_s)
 d_letter = {}
 d_word = {}
 d = {}
+len_all_letters = 0
 
 for i in lst_s :
     # count every word in s
@@ -16,6 +18,8 @@ for i in lst_s :
 
     for j in i :
         # count every letter in s
+        len_all_letters += 1
+        
         if j not in d_letter :
             d_letter.update( {j : 1} )
         else :
@@ -25,6 +29,8 @@ for i in lst_s :
 # print(d_letter)
 
 d = {
+    'all words' : len_all_words,
+    'all letters' : len_all_letters,
     'd_letter' : d_letter,
     'd_word' : d_word
 }

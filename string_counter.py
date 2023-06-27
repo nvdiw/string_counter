e3cfccdcb307fab3305f819eac2d09c0e8bf3b2a
nvdiw <nouraeinavid@gmail.com>
2023-06-27 16:25:04 +0430

    every letter and every word now Can counts

--- string_counter.py
+++ string_counter.py
@@ -1,1 +1,24 @@
-print('created')
+s = 'hi my name is navid and i want to test this string .'
+lst_s = s.split()
+d_letter = {}
+d_word = {}
+
+print(lst_s)
+
+for i in lst_s :
+    # count every word in s
+    if i not in d_word :
+        d_word.update( {i : 1} )
+    else :
+        d_word[i] += 1
+
+
+    for j in i :
+        # count every letter in s
+        if j not in d_letter :
+            d_letter.update( {j : 1} )
+        else :
+            d_letter[j] += 1
+
+print(d_word)
+print(d_letter)
